7831c775d4cd97b42ddd1934a155f03b37687a24
gastontechera <gastontecheram@gmail.com>
2023-09-12 02:24:41 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -1,39 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
     // 1. Obtener el ID del producto en el local storage
-let productId = localStorage.getItem("productId");
-console.log(productId)
+    let productId = localStorage.getItem("productId");
+    console.log(productId)
 
 
     // 2. Fetch
-let productInfoUrl = PRODUCT_INFO_URL + productId + EXT_TYPE;
+    let productInfoUrl = PRODUCT_INFO_URL + productId + EXT_TYPE;
 
     // 3. Hacer la solicitud y mostrar la información
     fetch(productInfoUrl)
-    .then(response => response.json())
-    .then(data => {
+        .then(response => response.json())
+        .then(data => {
 
-        document.getElementById("productName").innerHTML = `${data.name}`;
-        document.getElementById("productDescription").innerHTML = data.description;
-        document.getElementById("productPrice").innerHTML = `${data.cost} ${data.currency}`;
-        document.getElementById("productSoldCount").innerHTML = `${data.soldCount}`;
-        document.getElementById("productCategory").innerHTML = `${data.category}`;
+            document.getElementById("productName").innerHTML = `${data.name}`;
+            document.getElementById("productDescription").innerHTML = data.description;
+            document.getElementById("productPrice").innerHTML = `${data.cost} ${data.currency}`;
+            document.getElementById("productSoldCount").innerHTML = `${data.soldCount}`;
+            document.getElementById("productCategory").innerHTML = `${data.category}`;
 
-        let imagesContainer = document.getElementById("productImage");
+            let imagesContainer = document.getElementById("productImage");
 
-data.images.forEach(imagen => {
-let img = document.createElement("img");
-img.src = imagen;
-img.alt = data.name; 
-img.classList.add("img-fluid", "border", "m-2"); 
+            data.images.forEach(imagen => {
+                let img = document.createElement("img");
+                img.src = imagen;
+                img.alt = data.name;
+                img.classList.add("img-fluid", "border", "m-2");
 
-// Crear un contenedor de columna para la imagen que se ajuste automáticamente
-let colDiv = document.createElement("div");
-colDiv.classList.add("col"); 
-colDiv.appendChild(img);
+                // Contenedor de columnas para las imágenes
+                let colDiv = document.createElement("div");
+                colDiv.classList.add("col");
+                colDiv.appendChild(img);
 
-imagesContainer.appendChild(colDiv);
-});
+                imagesContainer.appendChild(colDiv);
+            });
 
-    });
+        });
 });
