692924dbafb2e602b06e0ba852ddf3b02e4735e9
juanbaridon <juanbaridonjd@hotmail.com>
2023-10-13 22:21:46 -0300

    carrito funcional

--- js/cart.js
+++ js/cart.js
@@ -1,66 +1,104 @@
-// const cartInfoUrl = CART_INFO_URL + 25801 + EXT_TYPE;
 const cartProducts = document.getElementById('cartProducts');
+let cost;
 
-// cartIcon clase del boton
+// Function to add items to the local storage list, trigger by onclick on the cart buttons
+function addToCart(itemId) {
+  let cartList = JSON.parse(localStorage.getItem("cartList")) || []
 
-//Lista de productos guardados;
-// function showData1(dataArray) {
-//     dataArray.articles.forEach((element)=>{
-//         cartProducts.innerHTML +=`
-//             <div class="card bg-light m-3">
-//                 <img src="${element.image}" class="card-img-top" alt="imagen del producto">
-//                 <div class="card-body">
-//                 <h5 class="card-title text-center pb-2">${element.name}</h5>
-//                 <div class="card-text">
-//                     <p>Costo: ${element.unitCost + element.currency}</p>
-//                     <p>Cantidad: <input type="number" min="1" value="${element.count}" class="cartProductCount text-center"></p>
-//                     <p class="subTotal btn btn-success">Subtotal:  ${(element.unitCost * element.count) + element.currency}</p>
-//                 </div>
-//                 <a href="#" class="btn btn-primary">Comprar</a>
-//                 <a href="#" class="btn btn-danger">Quitar</a>
-//                 </div>
-//             </div>
-//             `
-//     })
-
-//     //Modificación del subtotal (temporal);
-//     let inputCount = document.querySelectorAll('.cartProductCount');
-//     let subTotal = document.querySelectorAll('.subTotal');
-//     let cantidades = []
-//     for (let i = 0; i < inputCount.length; i++) {
-//         cantidades.push(inputCount[i].value);
-//         inputCount[i].addEventListener('input', ()=>{
-//             cantidades[i] = inputCount[i].value;
-//             subTotal[i].innerHTML = 'Subtotal: '+dataArray.articles[i].unitCost * cantidades[i] + dataArray.articles[i].currency;
-//         })
-        
-//     }
-//     //Dark mode
-//     modeListado()
-// }
-
-// async function getJson() {
-//     try{
-//       const response = await fetch(cartInfoUrl);
-//       const json = await response.json();
-//       showData1(json);
-//     }
-//     catch (error){
-//       console.error('Error al solicitar los productos \n', error);
-//       divProductos.innerHTML = `
-//         <div class="bg-danger text-white text-center rounded p-4 m-4">
-//           <h5>Lo sentimos, ha ocurrido un error.</h5>
-//         </div>`
-//     }
-//   }
-//   getJson();
-
-  function addToCart(itemId) {
-    let cartList = JSON.parse(localStorage.getItem("cartList")) || []
-
-           if(!cartList.includes(itemId)) {
-      cartList.push(itemId)
-    }
-    
-    localStorage.setItem("cartList", JSON.stringify(cartList))
+  if(!cartList.includes(itemId)) {
+    cartList.push(itemId)
   }
+  
+  localStorage.setItem("cartList", JSON.stringify(cartList))
+}
+
+// Fetch to the local storage list for cart items
+function fetchCart() {
+  const cartList = JSON.parse(localStorage.getItem("cartList"))
+
+  cartList.forEach(element => {
+    fetch(PRODUCT_INFO_URL + element + EXT_TYPE)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response error');
+      }
+      return response.json();
+    })
+    .then (data => {
+      showCart(data);
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+      cartProducts.innerHTML = `
+      <div class="bg-danger text-white text-center rounded p-4 m-4">
+        <h5>Lo sentimos, ha ocurrido un error.</h5>
+      </div>`
+    })
+  });
+}
+
+ fetchCart()
+ 
+
+ function showCart(data) {
+   const cartProducts = document.getElementById("cartProducts");
+   const newRow = document.createElement("tr");
+
+   cost = parseFloat(data.cost);
+
+   newRow.innerHTML = ` 
+     <th scope="row"><img src="${data.images[0]}" style="height: 60px; min-width: 60px;" class="img-thumbnail" alt="imagen del producto"></th>  
+     <td>${data.name}</td>
+     <td>${data.currency} ${data.cost}</td>
+     <td><input min="0" name="quantity" value="1" type="number" oninput="updateSubtotal(this, ${cost})" class="form-control form-control-sm""></td>
+     <td><span class="currency">${data.currency}</span> <span class="subtotal">${cost}</span></td>
+     <td><button class="btn btn-danger" onclick="removeCartItem(this.parentNode.parentNode, '${data.id}')">Eliminar</button></td>
+   `;
+
+   cartProducts.appendChild(newRow); 
+   updateTotal();
+   modeListado();
+ }
+
+ function removeCartItem(row, id) {
+  const cartProducts = document.getElementById("cartProducts");
+  cartProducts.removeChild(row);
+
+  let cartList = JSON.parse(localStorage.getItem("cartList")) || []
+  const index = cartList.indexOf(id);
+  if (index !== -1) {
+    cartList.splice(index, 1);
+    localStorage.setItem("cartList", JSON.stringify(cartList));
+  }
+
+  updateTotal()
+}
+
+function updateSubtotal(input, cost) {
+  const quantity = parseInt(input.value);
+  const subtotal = quantity * cost;
+  const subtotalElement = input.closest("tr").querySelector(".subtotal");
+  subtotalElement.textContent = `${subtotal}`;
+
+  updateTotal()
+}
+
+function updateTotal() {
+  subtotalList = document.getElementsByClassName("subtotal")
+  let totalPreDelivery = 0
+
+
+  for (let i = 0; i < subtotalList.length; i++) {
+    const subtotalText = subtotalList[i].textContent;
+    const subtotalValue = parseFloat(subtotalText.replace(/[^\d.-]/g, ""));
+    totalPreDelivery += subtotalValue;
+  }
+
+  const selectedDelivery = document.querySelector('input[name="flexRadioDefault"]:checked');
+  const percentageDelivery = parseFloat(selectedDelivery.getAttribute("data-percentage"));
+
+   total = totalPreDelivery + (totalPreDelivery * (percentageDelivery / 100));
+
+  const finalPriceContainer = document.getElementById("finalPrice")
+  finalPriceContainer.textContent = total;
+}
